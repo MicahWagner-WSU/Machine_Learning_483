5890661c499ed586b5561b21b0061af947c45853
Micah Wagner <micahwagner@Micahs-MacBook-Pro.local>
2023-09-13 19:17:40 -0700

    added denominator and naive bayes

--- Project 1/bayes.py
+++ Project 1/bayes.py
@@ -1,66 +1,46 @@
 import numpy as np
 import pandas as pd
+import math 
 
 data = pd.read_csv( 'zoo.csv' )
-train = train = data.sample( frac = 0.7 )
+train = data.sample( frac = 0.7 )
 test = data.drop( train.index )
 
 # def main():
 
-# def NB_probability(c, dataframe):
+def calc_naive_bayes(c,row, dataframe):
+	class_type = dataframe["class_type"].value_counts()
+	nb = math.log(class_type[c] / class_type.sum(), 2)
+	features = dataframe.drop("animal_name", axis=1).iloc[row]
+	for label in features.index:
+		nb += math.log(calc_feature_given_class(label,c,features[label],dataframe))
+	return 2**nb
 
-# def denom(c, dataframe):
+def calc_denominator(row, dataframe):
+	denominator = 0
+	# print(type(dataframe["class_type"].value_counts()))
+	for current_class in dataframe["class_type"].value_counts().index:
+		denominator += calc_naive_bayes(int(current_class), row, dataframe)
+	print(denominator)
+	return denominator
+
 
 # calculates the probability of a feature given a class, so P(f | c)
-# if the user wants to do legs, you must specify legsX, X being the leg count
+# the user specified weather we are checking for f = true or f = false in the f_val arguemnt
+# f_val = 0 to signify the feature = false, and f_val = 1 to signify the feature = true 
+# if the user wants to do legs, you must specify the leg count in f_val
 # class must be an integer
-def calc_feature_given_class(f, c, dataframe):
+def calc_feature_given_class(f, c, f_val, dataframe):
 
-	alpha = 1
+	alpha = 0.01
 	dimensionality = 7
-	num_of_class_types = dataframe["class_type"].value_counts()
+	class_types = dataframe["class_type"].value_counts()
 	numerator = 0
 
-	if f[:-1] != "legs":
-		numerator = ((dataframe[f] == 1) & (dataframe["class_type"] == c)).sum() + alpha
-	else:
-		numerator = ((dataframe[f[:-1]] == int(f[-1])) & (dataframe["class_type"] == c)).sum() + alpha
+	numerator = ((dataframe[f] == f_val) & (dataframe["class_type"] == c)).sum() + alpha
 
-	denominator = num_of_class_types[c] + alpha * dimensionality
+	denominator = class_types[c] + alpha * dimensionality
 	return numerator / denominator
 
 
-
-
-# def calc_feature_class_prob(dataframe):
-# 	feature_table = {}
-
-
-# 	for column_name in dataframe.columns:
-
-# 		# avoid calculating feature prob for animal names and class type
-# 		# legs need to be calculated seperately since its not a binary option
-# 		if column_name not in ["animal_name", "legs", "class_type"]:
-# 			# Convert values to numeric type and then calculate the probability
-# 			numeric_values = pd.to_numeric(dataframe[column_name], errors='coerce')
-# 			feature_table[column_name] = numeric_values.sum() / (len(dataframe) - 1)
-
-# 		# Initialize legs columns
-# 		unique_legs = dataframe["legs"].unique()
-# 		for leg_number in unique_legs:
-# 		    feature_table["legs" + str(leg_number)] = 0
-
-# 		# Count the occurrences of each 'legsX' value
-# 		for leg_number in dataframe["legs"]:
-# 			feature_table["legs" + str(leg_number)] += 1
-
-# 		# find the probability of having each leg type
-# 		for leg_number in unique_legs:
-# 			col_name = "legs" + str(leg_number)
-# 			feature_table[col_name] /= (len(dataframe.index) - 1)
-
-
-
-# 	return feature_table
-
-print(calc_feature_given_class("eggs", 1, train))
+print(calc_naive_bayes(1, 3, train) / calc_denominator(3, train))
