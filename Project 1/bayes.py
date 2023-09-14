67d25cc1357bb8bb0f16dec9eda4ca861ec4cb45
Micah Wagner <micahwagner@wless-user-10190255135.vancouver.wsu.edu>
2023-09-14 13:15:51 -0700

    added predict method, crashes sometimes

--- Project 1/bayes.py
+++ Project 1/bayes.py
@@ -6,22 +6,40 @@ data = pd.read_csv( 'zoo.csv' )
 train = data.sample( frac = 0.7 )
 test = data.drop( train.index )
 
-# def main():
+def main():
+
+	for i in test.index:
+		prediction = predict(i, test)
+
+
+		
+	
+def predict(row, dataframe):
+	class_types = train["class_type"].value_counts()
+	highest_prediction = 0;
+	class_type = -1
+	for i in class_types.index:
+		prediction = calc_naive_bayes(i, row, dataframe) / calc_denominator(row, dataframe)
+		if (prediction > highest_prediction):
+			highest_prediction = prediction
+			class_type = i
+		else:
+			continue
+	return [highest_prediction, class_type]
 
 def calc_naive_bayes(c,row, dataframe):
 	class_type = dataframe["class_type"].value_counts()
 	nb = math.log(class_type[c] / class_type.sum(), 2)
 	features = dataframe.drop("animal_name", axis=1).iloc[row]
 	for label in features.index:
-		nb += math.log(calc_feature_given_class(label,c,features[label],dataframe))
+		nb += math.log(calc_feature_given_class(label,c,features[label]))
 	return 2**nb
 
 def calc_denominator(row, dataframe):
 	denominator = 0
-	# print(type(dataframe["class_type"].value_counts()))
-	for current_class in dataframe["class_type"].value_counts().index:
+	class_types = train["class_type"].value_counts()
+	for current_class in class_types.index:
 		denominator += calc_naive_bayes(int(current_class), row, dataframe)
-	print(denominator)
 	return denominator
 
 
@@ -30,17 +48,18 @@ def calc_denominator(row, dataframe):
 # f_val = 0 to signify the feature = false, and f_val = 1 to signify the feature = true 
 # if the user wants to do legs, you must specify the leg count in f_val
 # class must be an integer
-def calc_feature_given_class(f, c, f_val, dataframe):
+def calc_feature_given_class(f, c, f_val):
 
 	alpha = 0.01
 	dimensionality = 7
-	class_types = dataframe["class_type"].value_counts()
+	class_types = train["class_type"].value_counts()
 	numerator = 0
 
-	numerator = ((dataframe[f] == f_val) & (dataframe["class_type"] == c)).sum() + alpha
+	numerator = ((train[f] == f_val) & (train["class_type"] == c)).sum() + alpha
 
 	denominator = class_types[c] + alpha * dimensionality
 	return numerator / denominator
 
-
+print(test)
+print(predict(1,test))
 print(calc_naive_bayes(1, 3, train) / calc_denominator(3, train))
