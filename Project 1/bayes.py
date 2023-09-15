b5ec48b7d620f09ca8fa0b469f82ec1c2cb0dca5
Micah Wagner <micahwagner@Micahs-MacBook-Pro.local>
2023-09-15 12:26:23 -0700

    finished I think

--- Project 1/bayes.py
+++ Project 1/bayes.py
@@ -1,45 +1,64 @@
 import numpy as np
 import pandas as pd
 import math 
+import sys
 
 data = pd.read_csv( 'zoo.csv' )
 train = data.sample( frac = 0.7 )
 test = data.drop( train.index )
 
+alpha = 0.01
+dimensionality = 7
+
 def main():
+	predicted_class = []
+	probability = []
+	correct = []
 
-	for i in test.index:
+	for i in range(0, len(test.index)):
 		prediction = predict(i, test)
-
-
-		
+		predicted_class.append(prediction[1])
+		probability.append(prediction[0])
+		correct.append(prediction[2])
+	test["predicted"] = predicted_class
+	test["probability"] = probability
+	test["correct?"] = correct
+	test.to_csv(sys.stdout, index=False)
+	
 	
 def predict(row, dataframe):
-	class_types = train["class_type"].value_counts()
-	highest_prediction = 0;
+	class_types = data["class_type"].unique()
+	actual_class_type = dataframe.iloc[row].loc["class_type"]
+	highest_prediction = 0
 	class_type = -1
-	for i in class_types.index:
+	correct = "?"
+	for i in class_types:
 		prediction = calc_naive_bayes(i, row, dataframe) / calc_denominator(row, dataframe)
 		if (prediction > highest_prediction):
 			highest_prediction = prediction
 			class_type = i
 		else:
 			continue
-	return [highest_prediction, class_type]
+	if(class_type == actual_class_type):
+		correct = "CORRECT"
+	else:
+		correct = "WRONG"
+	return [highest_prediction, class_type, correct]
 
 def calc_naive_bayes(c,row, dataframe):
 	class_type = dataframe["class_type"].value_counts()
-	nb = math.log(class_type[c] / class_type.sum(), 2)
+	class_count = (train["class_type"] == c).sum()
+	nb = math.log((class_count + alpha)/ (train["class_type"].value_counts().sum() + alpha * dimensionality), 2)
 	features = dataframe.drop("animal_name", axis=1).iloc[row]
 	for label in features.index:
-		nb += math.log(calc_feature_given_class(label,c,features[label]))
+		nb += math.log(calc_feature_given_class(label,int(c),features[label]),2)
 	return 2**nb
 
 def calc_denominator(row, dataframe):
 	denominator = 0
-	class_types = train["class_type"].value_counts()
-	for current_class in class_types.index:
-		denominator += calc_naive_bayes(int(current_class), row, dataframe)
+	class_types = data["class_type"].unique()
+	for current_class in class_types:
+		denominator += calc_naive_bayes(current_class, row, dataframe)
 	return denominator
 
 
@@ -49,9 +68,6 @@ def calc_denominator(row, dataframe):
 # if the user wants to do legs, you must specify the leg count in f_val
 # class must be an integer
 def calc_feature_given_class(f, c, f_val):
-
-	alpha = 0.01
-	dimensionality = 7
 	class_types = train["class_type"].value_counts()
 	numerator = 0
 
@@ -60,6 +76,4 @@ def calc_feature_given_class(f, c, f_val):
 	denominator = class_types[c] + alpha * dimensionality
 	return numerator / denominator
 
-print(test)
-print(predict(1,test))
-print(calc_naive_bayes(1, 3, train) / calc_denominator(3, train))
+main()
